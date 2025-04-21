7f7b10e1b421f0b9e597061babd235f3b9be0944
MDmitriy74 <dmostovov74@gmail.com>
2025-04-21 13:06:14 +0500

    add perexod to level

--- The-Lost-Treasure/Assets/Cainos/Pixel Art Top Down - Basic/Script/PortalToLevel.cs
+++ The-Lost-Treasure/Assets/Cainos/Pixel Art Top Down - Basic/Script/PortalToLevel.cs
@@ -1,14 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
 public class PortalToLevel : MonoBehaviour
 {
-    public int SceenNumber;
+    public int _SceenNumber;
+    private Camera mainCamera;
+    private Vector3 originalCameraPosition;
+    private bool isTriggered = false;
 
-    private void OnTriggerEnter2D(Collider2D other)
+
+    public float shakeDuration = 5f; // Длительность тряски экрана
+    public float shakeMagnitude = 0.1f; // Интенсивность тряски экрана
+    public float fadeDuration = 5f; // Длительность затемнения
+    public GameObject canvas;
+
+    void Start()
     {
-        Debug.Log("teleport to " + SceenNumber);
-        SceneManager.LoadScene(SceenNumber);
+        // Получаем ссылку на главную камеру
+        mainCamera = Camera.main;
+        if (mainCamera == null)
+        {
+            Debug.LogError("Main Camera not found in the scene!");
+            enabled = false; // Отключаем скрипт, чтобы избежать ошибок
+            return;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("teleport to " + _SceenNumber);
+        if (other.CompareTag("Player") && !isTriggered)
+        {
+            isTriggered = true;
+            originalCameraPosition = mainCamera.transform.position;
+            StartCoroutine(TriggerSequence());
+        }
+    }
+
+    IEnumerator TriggerSequence()
+    {
+        StartCoroutine(FadeOut());
+        StartCoroutine(ShakeCamera());
+
+        // 3. Ожидание окончания эффектов
+        yield return new WaitForSeconds(Mathf.Max(shakeDuration, fadeDuration)); // Ждем окончания самого длинного эффекта
+
+        SceneManager.LoadScene(_SceenNumber);
+    }
+
+    // Тряска камеры
+    IEnumerator ShakeCamera()
+    {
+        var elapsed = 0.0;
+
+        while (elapsed < shakeDuration)
+        {
+            float x = Random.Range(-1f, 1f) * shakeMagnitude;
+            float y = Random.Range(-1f, 1f) * shakeMagnitude;
+
+            mainCamera.transform.position = originalCameraPosition + new Vector3(x, y, 0); // Трясем только по X и Y
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+
+    //Затемнение экрана
+    IEnumerator FadeOut()
+    {
+
+        float elapsedTime = 0.0f;
+        while (elapsedTime < fadeDuration)
+        {
+            canvas.GetComponent<SpriteRenderer>().color =
+                new Color(0, 0, 0, (elapsedTime / fadeDuration));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
     }
 }
+
